bc8579b0d917f3d86ceab3972891e29ece5ed28a
Matper17 <matiasperalta992@gmail.com>
2023-11-06 22:54:45 -0300

    Implementación LOGIN

---
+++ app.js
@@ -0,0 +1,49 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import {__dirname} from "./utils.js"
+import handlebars from "express-handlebars"
+import viewsRouter from "./src/routes/views.router.js"
+import cookieRouter from "./src/routes/cookie.router.js"
+import sessionsRouter from "./src/routes/sessions.router.js"
+import session from "express-session"
+import "./src/db/configDB.js"
+import fileStore from "session-file-store"
+const FileStore = fileStore(session); 
+import MongoStore from "connect-mongo"
+import  path  from "path"
+const app = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser("SecretCookie"))
+
+//Mongo DB
+
+const URI = "mongodb+srv://clientUser:coderhouse23@tiendavirtualcluster.j1fv3uh.mongodb.net/?retryWrites=true&w=majority"
+app.use(
+    session({
+        store: new MongoStore({
+            mongoUrl: URI,
+        }),
+        secret: "secretSession",
+        cookie: {maxAge: 60000}
+    })
+)
+
+//Handlebars 
+
+app.engine("handlebars", handlebars.engine())
+app.set("views", path.join(__dirname + "views"))
+app.set("view engine", "handlebars")
+
+//Routes
+
+app.use("/", viewsRouter)
+app.use("/api/cookie", cookieRouter)
+app.use("/api/sessions", sessionsRouter)
+
+
+app.listen(8080, () =>{
+    console.log("Escuchando al puerto 8080")
+})
---
+++ src/routes/views.router.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+
+const router = Router(); 
+
+router.get("/login", (req, res) =>{
+    if(req.session.user){
+        return res.redirect("/profile")
+    }
+    res.render("login")
+})
+
+router.get("/profile", (req, res) =>{
+    if(!req.session.user){
+        return res.redirect("/login")
+    }
+    console.log(req.session.user)
+    res.render("profile", {user: req.session.user})
+})
+
+export default router; 
